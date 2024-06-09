d3d44e5336cf8bbc743e0d15ad647853447ac3b0
Underemployed <underemployed871@gmail.com>
2024-06-09 19:37:29 +0530

    stuck switching to spotipy

--- spotify.py
+++ spotify.py
@@ -1,2 +1,38 @@
 from secret import GOOGLE_API_KEY, SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET
+import base64
+import json
+import requests
+def get_token():
 
+    auth_string = SPOTIFY_CLIENT_ID + ":" + SPOTIFY_CLIENT_SECRET
+    auth_bytes = auth_string.encode("utf-8")
+    auth_base64 = base64.b64encode(auth_bytes).decode("utf-8")
+
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": "Basic " + auth_base64,
+        "Content-type": "application/x-www-form-urlencoded"
+    }
+    data = {"grant_type": "client_credentials"}
+    result = requests.post(url, headers=headers, data=data)
+    json_result = json.loads(result.content)
+    token = json_result["access_token"]
+    return token
+
+
+token   = get_token()
+
+def get_auth_header(token):
+    return {"Authorization": "Bearer " + token}
+
+
+def get_current_user_id(token):
+    url = "https://api.spotify.com/v1/me"
+    headers = get_auth_header(token)
+    response = requests.get(url, headers=headers)
+    json_response = response.json()
+    return json_response
+
+token = get_token()
+user_id = get_current_user_id(token)
+print(user_id)
