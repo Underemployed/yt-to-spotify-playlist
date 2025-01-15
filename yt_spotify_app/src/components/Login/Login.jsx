ec475455bc4d4d50c48eaaf15ee68f1df31efd64
Neha-Ajith-G <120302847+Neha-Ajith-G@users.noreply.github.com>
2025-01-15 09:26:58 +0530

    deleted current login

--- yt_spotify_app/src/components/Login/Login.jsx
+++
@@ -1,9 +0,0 @@
-const Login = () => {
-    return (
-      <div style={{ textAlign: "center", marginTop: "20px" }}>
-        <h2>Login Component</h2>
-        <p>Please log in to continue.</p>
-      </div>
-    );
-  };
-  export default Login;
